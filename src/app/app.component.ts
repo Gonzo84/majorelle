8f4eb6e16c356c541ff4117de2fc90bcef6e0675
@GonzoKg <krsticm@ymail.com>
2019-01-19 13:42:44 +0100

    styling sidebars

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import {Component, ViewChild} from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['../sass-files/components/side-bar/side-bar.scss']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   @ViewChild('leftSideDrawer') public leftSideDrawer: any;
