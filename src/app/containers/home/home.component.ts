1b6a611e99dc64da7fb6a9a328bb51401127e96c
@GonzoKg <krsticm@ymail.com>
2019-01-19 17:41:51 +0100

    animation home page

--- src/app/containers/home/home.component.ts
+++ src/app/containers/home/home.component.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, HostListener, ViewChild} from '@angular/core';
+import {trigger, state, style, transition, animate} from '@angular/animations';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  animations: [
+    trigger('scrollAnimation', [
+      state('show', style({
+        opacity: 1
+      })),
+      state('hide', style({
+        opacity: 0
+      })),
+      transition('show => hide', animate('700ms ease-out')),
+      transition('hide => show', animate('700ms ease-in'))
+    ])
+  ]
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
+  @ViewChild('whiteContainer') whiteContainer: ElementRef;
+  state = 'hide';
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
   }
 
+
+  @HostListener('window:scroll', ['$event'])
+  checkScroll() {
+    const viewHeight = window.innerHeight;
+    const scrollPosition = window.pageYOffset;
+    const whiteContainerPosition = this.whiteContainer.nativeElement.offsetTop;
+    const heightSum = viewHeight + scrollPosition;
+
+    if (heightSum > whiteContainerPosition && scrollPosition < viewHeight) {
+      this.state = 'show';
+    } else {
+      this.state = 'hide';
+    }
+  }
 }
