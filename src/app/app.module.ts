1597656e00b57cac16f95b1534ed46d85610c563
@GonzoKg <krsticm@ymail.com>
2019-02-16 14:13:27 +0100

    news & events

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,38 +1,42 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MaterialModule} from './material.module';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MaterialModule} from './material.module';
 
-import { FlexLayoutModule } from '@angular/flex-layout';
-import { ScrollToModule } from '@nicky-lenaers/ngx-scroll-to';
+import {FlexLayoutModule} from '@angular/flex-layout';
+import {ScrollToModule} from '@nicky-lenaers/ngx-scroll-to';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HomeComponent } from './containers/home/home.component';
-import { KitchensComponent } from './containers/kitchens/kitchens.component';
-import { BathroomsComponent } from './containers/bathrooms/bathrooms.component';
-import { BedroomsComponent } from './containers/bedrooms/bedrooms.component';
-import { AppFooterComponent } from './components/footer/footer';
+import {AppRoutingModule} from './app-routing.module';
+
+import {AppComponent} from './app.component';
+import {AppFooterComponent} from './components/footer/footer';
+import {BathroomsComponent} from './containers/bathrooms/bathrooms.component';
+import {BedroomsComponent} from './containers/bedrooms/bedrooms.component';
+import {HomeComponent} from './containers/home/home.component';
+import {KitchensComponent} from './containers/kitchens/kitchens.component';
+import {NewsCardComponent} from './components/news-card/news-card';
 
 @NgModule({
   declarations: [
     AppComponent,
     AppFooterComponent,
+    BathroomsComponent,
+    BedroomsComponent,
     HomeComponent,
     KitchensComponent,
-    BathroomsComponent,
-    BedroomsComponent
+    NewsCardComponent
   ],
   imports: [
-    BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MaterialModule,
+    BrowserModule,
     FlexLayoutModule,
+    MaterialModule,
     ScrollToModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ src/app/components/news-card/news-card.ts
@@ -0,0 +1,13 @@
+import {Component, Input} from '@angular/core';
+
+@Component({
+  selector: 'app-news-card',
+  templateUrl: './news-card.html',
+  styleUrls: ['./news-card.scss']
+})
+export class NewsCardComponent {
+  @Input() data: any;
+
+  constructor() {
+  }
+}
